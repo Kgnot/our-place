8570bae553af8db49659d0a00365ae18fac6c6d1
Kgnot <hricaurtem@proton.me>
2026-08-04 16:41:30 -0500

    add JPA repository and entity scanning support for application

--- app/src/main/java/org/our_place/ourplacebackend/OurPlaceBackendApplication.java
+++ app/src/main/java/org/our_place/ourplacebackend/OurPlaceBackendApplication.java
@@ -2,10 +2,14 @@ package org.our_place.ourplacebackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.our_place")
+@EnableJpaRepositories(basePackages = "org.our_place")
+@EntityScan(basePackages = "org.our_place")
 public class OurPlaceBackendApplication {
 
     public static void main(String[] args) {
